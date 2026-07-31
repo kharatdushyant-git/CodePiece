d0572a5e4118ab250c9b3c1136d39c05dc8ed67c
kharatdushyant-git <kharatdushyant9@gmail.com>
2026-07-31 19:20:35 +0530

    Update PROgram0003.cpp

--- 01_Rookie_0800/0001_4A_Watermelon/PROgram0003.cpp
+++ 01_Rookie_0800/0001_4A_Watermelon/PROgram0003.cpp
@@ -10,9 +10,90 @@
 /////////////////////////////////////////////////////////////////////////
 //
 //  Header Files
-//  bits/stdc++.h : Includes all standard C++ libraries.
+//  bits/stdc++.h : Includes all standard C++ standard libraries.
 //
 /////////////////////////////////////////////////////////////////////////
 
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
+
+/////////////////////////////////////////////////////////////////////////
+//
+//  User Defined Data Types
+//  BOOL : Represents Boolean values using bool type.
+//
+//  User Defined Constants
+//
+//  TRUE  : Represents logical true.
+//  FALSE : Represents logical false.
+//
+/////////////////////////////////////////////////////////////////////////
+
+typedef bool BOOL;
+
+#define TRUE true
+#define FALSE false
+
+/////////////////////////////////////////////////////////////////////////
+//
+//  Function Name : CheckWaterMelon
+//  Description   : Checks whether the watermelon can be divided into
+//                  two even positive parts.
+//  Input         : Integer
+//  Output        : BOOL
+//  Author        : ArcaneStage
+//
+/////////////////////////////////////////////////////////////////////////
+
+BOOL CheckWaterMelon(int iNo)
+{
+    if ((iNo > 2) && (iNo % 2 == 0))
+    {
+        return TRUE;
+    }
+    else
+    {
+        return FALSE;
+    }
+}
+
+/////////////////////////////////////////////////////////////////////////
+//
+//  Function Name : main
+//  Description   : Entry point of the program. Accepts the weight of
+//                  the watermelon, invokes CheckWaterMelon(), and
+//                  displays the result.
+//  Input         : None
+//  Output        : Integer
+//  Author        : ArcaneStage
+//
+/////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    
+    // Improves the speed of input and output operations.
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+
+    // Logic
+    int iWeight = 0;
+    BOOL bRet = FALSE;
+
+    cin >> iWeight;
+
+    bRet = CheckWaterMelon(iWeight);
+
+    if (bRet == TRUE)
+    {
+        cout << "YES\n";
+    }
+    else
+    {
+        cout << "NO\n";
+    }
+
+    return 0;
+} give me same for c
